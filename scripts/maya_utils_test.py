ecff4107a089c923278364ccc9359b294e02572e
苍之幻灵 <www.cpcgskill.com>
2022-03-14 01:29:52 +0800

    编写了基础功能与基础的测试

---
+++ scripts/maya_utils_test.py
@@ -0,0 +1,60 @@
+# -*-coding:utf-8 -*-
+u"""
+:创建时间: 2022/3/13 18:28
+:作者: 苍之幻灵
+:我的主页: https://cpcgskill.com
+:QQ: 2921251087
+:爱发电: https://afdian.net/@Phantom_of_the_Cang
+:aboutcg: https://www.aboutcg.org/teacher/54335
+:bilibili: https://space.bilibili.com/351598127
+
+"""
+try:
+    import maya.standalone
+
+    maya.standalone.initialize(name='python')
+except:
+    pass
+
+from maya_utils import *
+
+
+@call_block
+def test():
+    if True:
+        if True:
+            if True:
+                if True:
+                    if True:
+                        if True:
+                            if True:
+                                if True:
+                                    if True:
+                                        if True:
+                                            if True:
+                                                if True:
+                                                    if True:
+                                                        if True:
+                                                            if True:
+                                                                if True:
+                                                                    if True:
+                                                                        if True:
+                                                                            if True:
+                                                                                if True:
+                                                                                    if True:
+                                                                                        if True:
+                                                                                            if True:
+                                                                                                if True:
+                                                                                                    if True:
+                                                                                                        if True:
+                                                                                                            if True:
+                                                                                                                if True:
+                                                                                                                    if True:
+                                                                                                                        if True:
+                                                                                                                            raise IndexError(
+                                                                                                                                "Test")
+
+
+
+test()
+raise IndexError("Test")
